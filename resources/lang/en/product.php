0e111efc5eb0815def462342b298d9efa70ebc1e
Agustin Bonilla <agustinbonillaeguren@gmail.com>
2022-10-18 18:30:15 -0300

    Se añade libreria de lenguaje para productos

---
+++ resources/lang/en/product.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Product Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create.success' => 'Product was added.',
+    'delete.success' => 'Product was removed.',
+    'delete.failed' => 'There was a problem removing the product.',
+
+];
---
+++ resources/lang/es/product.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Product Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'create.success' => 'Se añadió el producto.',
+    'delete.success' => 'Se eliminó el producto.',
+    'delete.failed' => 'Ocurrió un problema al eliminar el producto.',
+
+];
