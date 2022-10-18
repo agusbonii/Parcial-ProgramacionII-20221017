ff4a05f752222f38f618c754b8aad438990f1166
Agustin Bonilla <agustinbonillaeguren@gmail.com>
2022-10-18 05:50:33 -0300

    Se ordenan las rutas

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,7 @@ Route::get('/', [ProductoController::class, "listAll"]) -> name("home");
 
 Route::get('/inicio', [ProductoController::class, "listAll"]) -> name("home");
 
-
 Route::get('/producto/{index}', [ProductoController::class, "listByID"]) -> name("verProducto");
-
 Route::get('/productos/nuevo', function () { return view("producto.nuevo");}) -> name("crearProducto");
 Route::post('/productos/nuevo', [ProductoController::class, "Create"]) -> name("crearProducto");
-
 Route::get('/productos', [ProductoController::class, "listAll"]) -> name("productos");
